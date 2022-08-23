a8bf63bee4b79ebfbb8384e5b0a6e493e9e9c222
Adel Shanan <adel.a.shaban@gmail.com>
2022-08-23 04:53:53 +0200

    Step 12 & Step 13

--- starter/app/src/main/java/com/udacity/shoestore/screens/MainActivity.kt
+++ starter/app/src/main/java/com/udacity/shoestore/screens/MainActivity.kt
@@ -2,8 +2,15 @@ package com.udacity.shoestore.screens
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.onNavDestinationSelected
+import androidx.navigation.ui.setupWithNavController
 import com.udacity.shoestore.R
 import com.udacity.shoestore.databinding.ActivityMainBinding
 import com.udacity.shoestore.viewmodels.ShoesListViewModel
@@ -14,17 +21,32 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val binding : ActivityMainBinding = DataBindingUtil.setContentView(this,
+        val binding: ActivityMainBinding = DataBindingUtil.setContentView(
+            this,
             R.layout.activity_main
         )
         Timber.plant(Timber.DebugTree())
 
         ShoesListViewModelHelper.generateDummyData(this, 10)
 
+        //setHasOptionsMenu(true)
         // Only for tracing
         ShoesListViewModelHelper.traceViewModel(this)
 
+        setSupportActionBar(binding.toolbar)
+        val navController = findNavController(R.id.myNavHostFragment)
+        val config = AppBarConfiguration(navController.graph)
+        binding.toolbar.setupWithNavController(navController, config)
     }
 
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.options_menu, menu)
+        return true
+    }
 
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val navController = findNavController(R.id.myNavHostFragment)
+        return item.onNavDestinationSelected(navController) ||
+                super.onOptionsItemSelected(item)
+    }
 }
